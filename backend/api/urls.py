0901a401ca7e4e786349d60f9fc2dd69cf772083
AndrewVanHoveln <andrewvanhoveln2001@gmail.com>
2024-04-17 15:27:06 -0500

    Changing to Java Spring Boot api

--- backend/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('entries/', views.FoodListCreate.as_view(), name = 'food-list'),
-    path('notes/delete/<int:pk>/', views.FoodDelete.as_view(), name = 'delete-food'),
-]
